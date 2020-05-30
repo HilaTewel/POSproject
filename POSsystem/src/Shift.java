214aa7a660291efcc17df3ec0115fd5d47896c74
Gal919 <galraidi00@gmail.com>
2020-05-30 14:23:41 +0300

    test

--- POSsystem/src/Shift.java
+++ POSsystem/src/Shift.java
@@ -3,9 +3,12 @@ import java.util.List;
 
 public class Shift {
     private List<POS> activePOS;
+
     private List<Worker> workers;
     private Manager manager;
     private List<Deal> deals;
     private float totalIncome;
     private Date date;
 }
+
+
